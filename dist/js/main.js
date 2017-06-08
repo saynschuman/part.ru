375a89850cfb522998206667264477e5fe369a79
saynschuman <saynschuman@gmail.com>
2017-06-08 13:59:33 +0300

    ui-slider finished

--- dist/js/main.js
+++ dist/js/main.js
@@ -321,4 +321,20 @@
 		margin: 16
 	})
 
+	$("#slider").slider({
+	    min: 0,
+	    max: 1000,
+	    values: [100,800],
+	    range: true,
+	    stop: function(event, ui) {
+	        jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
+	        jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
+	    },
+	    slide: function(event, ui){
+	        jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
+	        jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
+	    }
+	});
+
+
 })(jQuery);
